488486ee2b2ecdb6cddc5f3c31aa5e0d39f8a6b8
Suraj <unofficialartist3@gmail.com>
2023-02-05 01:32:35 +0530

    chagnes

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -22,24 +22,24 @@ function PostItem({ post, admin = false }) {
           <h1 className='border-l-2 text-base sm:text-2xl   border-green-400 pl-2 mt-2 mainfont'>
             <a className='' >{post.title}</a>
           </h1 >
-          <p style={{ "width": "60%" }} className='p-2 overflow-hidden whitespace-nowrap text-gray-600 '>{post.description}
+          <p style={{ "width": "60%" }} className='p-2 flex-col whitespace-wrap text-gray-600 '>{post.description}
           </p>
-          <ul className='decoration-none text-sm flex flex-row  overflow-hidden mr-2 w-[80%]  '>
+          <ul className='decoration-none text-sm flex flex-wrap overflow-hidden mr-2 w-[80%]  '>
           {post?.tag?.map((hash) => {
             return (
               <>
-              <li className='bg-blue-100 m-2 rounded-lg p-1 text-xs text-gray-800 '>{hash}</li> 
+              <li className='bg-blue-100 m-1 rounded-lg p-1 text-xs text-gray-800 '>{hash}</li> 
               </>
             )
           }
             )}
 
-
-
-            <span className=" flex flex-row mt-3"> {post.heartCount || 0} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+            <span className=" flex flex-row mt-1"> {post.heartCount || 0} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
               <path fill-rule="evenodd" d="M11.47 4.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 01-1.06 1.06L12 6.31 8.78 9.53a.75.75 0 01-1.06-1.06l3.75-3.75zm-3.75 9.75a.75.75 0 011.06 0L12 17.69l3.22-3.22a.75.75 0 111.06 1.06l-3.75 3.75a.75.75 0 01-1.06 0l-3.75-3.75a.75.75 0 010-1.06z" clip-rule="evenodd" />
             </svg>
               Upvote</span>
+
+
           </ul>
         </span>
       </Link>
